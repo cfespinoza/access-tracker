f83f9fc0aa086d308ffcef22734fa58598f54333
Charles A. Flores Espinoza <cfloresespinoza@gmail.com>
2015-03-01 02:08:22 +0100

    First view stablished. Almost finish second view

--- gen/com/caflores/access_tracker/R.java
+++ gen/com/caflores/access_tracker/R.java
@@ -25,16 +25,30 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f070000;
-        public static final int button2=0x7f070001;
+        public static final int buttonIn=0x7f070002;
+        public static final int buttonOut=0x7f070003;
+        public static final int confirm_enter=0x7f070006;
+        public static final int driver_name=0x7f070005;
+        public static final int fullscreen_content=0x7f070000;
+        public static final int fullscreen_content_controls=0x7f070001;
+        public static final int registration_tag=0x7f070004;
     }
     public static final class layout {
         public static final int activity_tracker=0x7f030000;
+        public static final int goingin_view=0x7f030001;
+        public static final int goingout_view=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int buttonIn=0x7f050003;
+        public static final int buttonOut=0x7f050004;
+        public static final int button_send=0x7f050006;
+        public static final int confirm_enter=0x7f050009;
+        public static final int driver_name_field=0x7f050008;
         public static final int dummy_button=0x7f050001;
         public static final int dummy_content=0x7f050002;
+        public static final int edit_message=0x7f050005;
+        public static final int registration_tag=0x7f050007;
     }
     public static final class style {
         /** 
